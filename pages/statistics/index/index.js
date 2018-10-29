49bc323e0d839e37a79232d4409c9e5e9531013e
well kim <505144350@qq.com>
2018-10-29 11:25:43 +0800

    commit

---
+++ pages/statistics/index/index.js
@@ -0,0 +1,26 @@
+// pages/order/index/index.js
+const app = getApp()
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+  },
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+  },
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {}
+
+})
